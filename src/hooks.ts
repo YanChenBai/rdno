7c39a46ffa5a8de4911df3ad2e32fe53aa6cdacf
byc <bycrxemail@gmail.com>
2026-03-25 15:20:21 +0800

    refactor(hooks):  移除 try-catch 块

--- src/hooks.ts
+++ src/hooks.ts
@@ -20,51 +20,41 @@ export const resolve: ResolveHook = async (specifier, context, nextResolve) => {
   if (!parentURL || isBuiltin(specifier) || specifier.startsWith('node:'))
     return nextResolve(specifier, context);
 
-  try {
-    const parentPath = fileURLToPath(parentURL);
-    const lookupDir = path.dirname(parentPath);
+  const parentPath = fileURLToPath(parentURL);
+  const lookupDir = path.dirname(parentPath);
 
-    const resolution = await resolver.async(lookupDir, specifier);
+  const resolution = await resolver.async(lookupDir, specifier);
 
-    if (resolution.path) {
-      return {
-        url: pathToFileURL(resolution.path).href,
-        shortCircuit: true,
-      };
-    }
-  } catch (error) {
-    console.error(`[RDNO] Error resolving ${specifier}: ${error}`);
+  if (resolution.path) {
+    return {
+      url: pathToFileURL(resolution.path).href,
+      shortCircuit: true,
+    };
   }
 
   return nextResolve(specifier, context);
 };
 
 export const load: LoadHook = async (url, context, nextLoad) => {
-  if (url.startsWith('file://')) {
-    const filePath = fileURLToPath(url);
-    const ext = path.extname(filePath);
+  if (!url.startsWith('file://')) return nextLoad(url, context);
 
-    if (['.ts', '.tsx', '.mts'].includes(ext)) {
-      try {
-        const source = await readFile(filePath, 'utf8');
+  const filePath = fileURLToPath(url);
+  const ext = path.extname(filePath);
 
-        const result = await transform(filePath, source, {
-          sourcemap: true,
-          sourceType: 'module',
-          target: 'esnext',
-          typescript: {},
-        });
+  if (!['.ts', '.tsx', '.mts'].includes(ext)) return nextLoad(url, context);
 
-        return {
-          format: 'module',
-          source: result.code,
-          shortCircuit: true,
-        };
-      } catch (error) {
-        console.error(`[RDNO] Failed to load ${url}:`, error);
-      }
-    }
-  }
+  const source = await readFile(filePath, 'utf8');
 
-  return nextLoad(url, context);
+  const result = await transform(filePath, source, {
+    sourcemap: true,
+    sourceType: 'module',
+    target: 'esnext',
+    typescript: {},
+  });
+
+  return {
+    format: 'module',
+    source: result.code,
+    shortCircuit: true,
+  };
 };
